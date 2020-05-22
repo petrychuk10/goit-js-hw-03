a26eb1769f2cc7eb77fc6fe70a38275039a6d0b7
petrychuk10 <petrychuk10@gmail.com>
2020-05-22 10:48:30 +0300

    task-4

---
+++ task-3.js
@@ -0,0 +1,33 @@
+const findBestEmployee = function (employees) {
+  for (const sales of Object.values(employees)) {
+    if (Object.values(employees).sales === Math.max(sales)) {
+      return Object.keys(employees);
+    }
+  }
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  })
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  })
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  })
+); // lux
